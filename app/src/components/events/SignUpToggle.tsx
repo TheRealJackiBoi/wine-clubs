c6b8dc03fed35c406b33758956e0f9944b53169c
TheRealJackiBoi <jslam@oulund.dk>
2024-10-17 00:03:14 +0200

    add signup for event api route

--- app/src/components/events/SignUpToggle.tsx
+++ app/src/components/events/SignUpToggle.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import { Button } from '@chakra-ui/react'
+import { Button, useToast } from '@chakra-ui/react'
 import axios from 'axios'
+import { useRouter } from 'next/navigation'
 
 const SignUpToggle = ({
   isSignedUp,
@@ -14,12 +15,54 @@ const SignUpToggle = ({
   eventId: string
   userEmail: string
 }) => {
-  const toggleSignUp = async () => {
-    const result = await axios.patch(`/clubs/${clubId}/events/${eventId}`, {
-      userEmail: userEmail,
-    })
+  const toast = useToast()
+  const router = useRouter()
 
-    console.log(result)
+  const toggleSignUp = async () => {
+    try {
+      const result = await axios.patch(
+        `/api/clubs/${clubId}/events/${eventId}/toggle-signup`,
+        {
+          userEmail: userEmail,
+        },
+      )
+
+      const data = result.data
+
+      if (data.success) {
+        toast({
+          title: 'Success',
+          description: data.message,
+          status: 'success',
+          duration: 5000,
+          isClosable: true,
+        })
+        router.refresh()
+      } else {
+        toast({
+          title: 'Error',
+          description: data.message,
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+        })
+      }
+    } catch (error) {
+      console.error(error)
+
+      const errorMessage =
+        axios.isAxiosError(error) && error.response
+          ? error.response.data.message
+          : 'An unexpected error occurred.'
+
+      toast({
+        title: 'Error',
+        description: errorMessage,
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+      })
+    }
   }
 
   return (
