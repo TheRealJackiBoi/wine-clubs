e5a6d48692224beb4ea3bd4a3acdb22317faa6b1
snadering <cph-sr319@cphbusiness.dk>
2024-10-18 00:09:45 +0200

    make club cards redirect to /clubs/[id]

--- app/src/app/clubs/page.tsx
+++ app/src/app/clubs/page.tsx
@@ -15,6 +15,7 @@ import { MdPeople } from 'react-icons/md'
 import prisma from '@/lib/prisma'
 import { WineClub } from '@prisma/client'
 import { notFound } from 'next/navigation'
+import Link from 'next/link' // Import Link for navigation
 
 const ClubsPage: FC = async () => {
   const clubs: WineClub[] = await prisma.wineClub.findMany({
@@ -33,42 +34,45 @@ const ClubsPage: FC = async () => {
       </Heading>
       <SimpleGrid columns={[1, 2, 3]} spacing={8}>
         {clubs.map((club) => (
-          <Box
-            key={club.id}
-            borderWidth='1px'
-            borderRadius='lg'
-            overflow='hidden'
-            boxShadow='lg'
-            _hover={{ boxShadow: 'xl' }}
-          >
-            <Image
-              src={club.image}
-              alt={club.name}
-              height='200px'
-              objectFit='cover'
-            />
-            <VStack align='stretch' padding={4}>
-              <Heading as='h2' size='lg'>
-                {club.name}
-              </Heading>
-              <Text>{club.description}</Text>
-              <HStack justify='space-between' mt={4}>
-                <HStack>
-                  <MdPeople />
-                  <Text>{club.members.length} Members</Text>
+          <Link key={club.id} href={`/clubs/${club.id}`}>
+            {' '}
+            {/* Wrap the Box with Link */}
+            <Box
+              borderWidth='1px'
+              borderRadius='lg'
+              overflow='hidden'
+              boxShadow='lg'
+              _hover={{ boxShadow: 'xl' }}
+            >
+              <Image
+                src={club.image}
+                alt={club.name}
+                height='200px'
+                objectFit='cover'
+              />
+              <VStack align='stretch' padding={4}>
+                <Heading as='h2' size='lg'>
+                  {club.name}
+                </Heading>
+                <Text>{club.description}</Text>
+                <HStack justify='space-between' mt={4}>
+                  <HStack>
+                    <MdPeople />
+                    <Text>{club.members.length} Members</Text>
+                  </HStack>
+                  <AvatarGroup size='sm' max={3}>
+                    {club.members.map((member) => (
+                      <Avatar
+                        key={member.id}
+                        name={member.name}
+                        src={member.avatar}
+                      />
+                    ))}
+                  </AvatarGroup>
                 </HStack>
-                <AvatarGroup size='sm' max={3}>
-                  {club.members.map((member) => (
-                    <Avatar
-                      key={member.id}
-                      name={member.name}
-                      src={member.avatar}
-                    />
-                  ))}
-                </AvatarGroup>
-              </HStack>
-            </VStack>
-          </Box>
+              </VStack>
+            </Box>
+          </Link>
         ))}
       </SimpleGrid>
     </Box>
